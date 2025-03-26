dd786adf4b8ba031a3c83f54ced6e37e098fa758
Luis <famorim0888@gamil.com>
2025-03-26 11:50:49 -0300

    finalizando primeira parte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,19 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Menu } from "lucide-react";
+import { Github, Instagram, Linkedin, Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col ">
-      <header className="text-white flex justify-between p-8 fixed top-0 left-0 right-0  bg-[#121212] z-10">
+      <header className="text-white flex justify-between p-4 md:p-8 fixed top-0 left-0 right-0   z-10">
         <a href="#" className="text-2xl font-bold tracking-wide">
-          Luís <span className="text-cyan-400 shadow-cyan-500/50">Felipe</span>
+          Luís
+          <span className="text-cyan-400 drop-shadow-[0_0_0.3rem_#00B4D8]">
+            Felipe
+          </span>
         </a>
 
         <nav className="md:hidden">
@@ -20,55 +25,123 @@ export default function Home() {
             <DropdownMenuTrigger>
               <Menu className="text-cyan-400" />
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
+            <DropdownMenuContent className="border-transparent bg-[#121212]">
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Sobre</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
+              <DropdownMenuItem className="hover:border-b-2 text-white hover:text-cyan-400">
+                Sobre
+              </DropdownMenuItem>
+              <DropdownMenuItem className="hover:border-b-2 text-white hover:text-cyan-400">
+                Billing
+              </DropdownMenuItem>
+              <DropdownMenuItem className="hover:border-b-2 text-white hover:text-cyan-400">
+                Team
+              </DropdownMenuItem>
+              <DropdownMenuItem className="hover:border-b-2 text-white hover:text-cyan-400">
+                Subscription
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </nav>
 
         <nav className="md:flex gap-8 hidden font-bold">
-          <a href="#" className="hover:text-cyan-400 hover:border-b-4">
+          <a
+            href="#"
+            className="hover:text-cyan-400 hover:border-b-4   hover:drop-shadow-[0_0_0.3rem_#00B4D8]"
+          >
             Sobre
           </a>
-          <a href="#" className="hover:text-cyan-400 hover:border-b-4">
+          <a
+            href="#"
+            className="hover:text-cyan-400 hover:border-b-4   hover:drop-shadow-[0_0_0.3rem_#00B4D8]"
+          >
             Projetos
           </a>
-          <a href="#" className="hover:text-cyan-400 hover:border-b-4">
+          <a
+            href="#"
+            className="hover:text-cyan-400 hover:border-b-4   hover:drop-shadow-[0_0_0.3rem_#00B4D8]"
+          >
             Skills
           </a>
-          <a href="#" className="hover:text-cyan-400 hover:border-b-4">
+          <a
+            href="#"
+            className="hover:text-cyan-400 hover:border-b-4   hover:drop-shadow-[0_0_0.3rem_#00B4D8]"
+          >
             Contato
           </a>
         </nav>
       </header>
 
-      <section className="mt-24 px-8">
-        <h1>
-          Olá, eu sou <span>Luís</span>
-        </h1>
-        <p>
-          💻 Sou um desenvolvedor apaixonado por tecnologia e inovação! Com 21
-          anos, sou formado em Análise e Desenvolvimento de Sistemas pela UNIP e
-          atuo como Trainee Full Stack.
-        </p>
-        <p>
-          🚀 Minha jornada profissional é focada no desenvolvimento de software,
-          explorando tanto o back-end quanto o front-end. Busco constantemente
-          aprimorar minhas habilidades e aprender novas tecnologias para
-          entregar soluções eficientes e de alto impacto.
-        </p>
+      <section className=" overflow-auto mt-24 px-8  flex flex-col-reverse md:flex-row w-full  h-[80vh]">
+        <div className="flex md:w-[50%] items-center justify-center  ">
+          <div className="text-white gap-4 flex flex-col text-justify">
+            <h1 className="text-4xl font-bold">
+              HI, I'm
+              <span className="text-cyan-400 drop-shadow-[0_0_0.5rem_#00B4D8] ml-2">
+                Luís Felipe
+              </span>
+            </h1>
 
-        <p>
-          📈 Estou sempre em busca de desafios que me permitam crescer
-          profissionalmente e contribuir para projetos inovadores.
-        </p>
+            <p>
+              💻 Sou um desenvolvedor apaixonado por tecnologia e inovação! Com
+              21 anos, sou formado em Análise e Desenvolvimento de Sistemas pela
+              UNIP e atuo como Trainee Full Stack.
+            </p>
+            <p>
+              🚀 Minha jornada profissional é focada no desenvolvimento de
+              software, explorando tanto o back-end quanto o front-end. Busco
+              constantemente aprimorar minhas habilidades e aprender novas
+              tecnologias para entregar soluções eficientes e de alto impacto.
+            </p>
 
-        <div>
-          
+            <p>
+              📈 Estou sempre em busca de desafios que me permitam crescer
+              profissionalmente e contribuir para projetos inovadores.
+            </p>
+
+            <div className="flex gap-4">
+              <a
+                href="https://www.linkedin.com/in/luis-felipe-da-silva-amorim/"
+                target="_blank"
+              >
+                <Button className="hover:scale-150 cursor-pointer border-cyan-400 border-2  shadow-[0_0_15px_rgba(34,211,238,0.5)] hover:shadow-[0_0_25px_rgba(34,211,238,0.8)]">
+                  <Linkedin />
+                </Button>
+              </a>
+              <a href="https://github.com/luis0777" target="_blank">
+                <Button className="hover:scale-150 cursor-pointer border-cyan-400 border-2  shadow-[0_0_15px_rgba(34,211,238,0.5)] hover:shadow-[0_0_25px_rgba(34,211,238,0.8)]">
+                  <Github />
+                </Button>
+              </a>
+              <a
+                href="https://www.instagram.com/luisfelipe__7/"
+                target="_blank"
+              >
+                <Button className="hover:scale-150 cursor-pointer border-cyan-400 border-2  shadow-[0_0_15px_rgba(34,211,238,0.5)] hover:shadow-[0_0_25px_rgba(34,211,238,0.8)]">
+                  <Instagram />
+                </Button>
+              </a>
+            </div>
+
+            <a
+              href="https://drive.google.com/uc?export=download&id=1pMjzySmAFhUoGqiWJuLiJlqKacrqVupI"
+              download
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button className="bg-cyan-400 text-black shadow-cyan-400 border-cyan-400 border-2 cursor-pointer hover:bg-transparent hover:text-cyan-400  shadow-[0_0_15px_rgba(34,211,238,0.5)] hover:shadow-[0_0_25px_rgba(34,211,238,0.8)]">
+                Baixar Curriculo
+              </Button>
+            </a>
+          </div>
+        </div>
+        <div className=" md:w-[50%]  items-center justify-center  flex mb-8 mt-8 md:mb-0 md:mt-0">
+          <Image
+            src="/foto.jpg"
+            alt="Profile"
+            width={400}
+            height={400}
+            className="w-[250px] md:w-[400px] rounded-full border-2 border-cyan-400 shadow-[0_0_60px_rgba(34,211,238,0.8)] hover:shadow-[0_0_25px_rgba(34,211,238,0.8)]"
+          />
         </div>
       </section>
     </div>
