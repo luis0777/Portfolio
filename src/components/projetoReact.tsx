53921512d69b0f7bda3df3b294014a831b326254
Luis <famorim0888@gamil.com>
2025-03-28 10:29:20 -0300

    implementando projeto react

---
+++ src/components/projetoReact.tsx
@@ -0,0 +1,64 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
+
+export function ProjetoReact() {
+  return (
+    <Dialog>
+      <DialogTrigger className="p-8  cursor-pointer w-full max-w-[300px] border-2 rounded-3xl border-gray-500 hover:border-cyan-400 hover:shadow-[0_0_60px_rgba(34,211,238,0.8)] duration-200 hover:scale-90">
+        <div className="flex justify-center">
+          <Image src="/react.png" alt="java" width={130} height={300} />
+        </div>
+        <span className="flex justify-center text-white text-2xl font-bold">
+          Projetos em React
+        </span>
+      </DialogTrigger>
+      <DialogContent className="bg-[#080808] w-[340px] md:w-[600px] border-gray-500">
+        <DialogHeader>
+          <DialogTitle className="text-white flex justify-center">
+            Projetos em React
+          </DialogTitle>
+
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger className=" hover:text-cyan-400">
+                PET SHOP
+              </AccordionTrigger>
+              <AccordionContent className="">
+                <span className="font-bold text-lg text-cyan-400 ">
+                  Objetivo:
+                </span>
+                <p className="text-justify">
+                  Projeto sobre um site de petshop feito em react.
+                </p>
+                <a
+                  href="https://github.com/luis0777/PetShop-React"
+                  target="_blank"
+                  className="w-full flex justify-center"
+                >
+                  <Button className="mt-5 hover:scale-105 cursor-pointer border-cyan-400 border-2  shadow-[0_0_15px_rgba(34,211,238,0.5)] hover:shadow-[0_0_25px_rgba(34,211,238,0.8)]">
+                    <Github /> Repositorio
+                  </Button>
+                </a>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/projetos.tsx
+++ src/components/projetos.tsx
@@ -12,6 +12,7 @@ import Image from "next/image";
 import { ProjetoJava } from "./projetoJava";
 import { ProjetoCsharp } from "./projetoCsharp";
 import { ProjetoC } from "./projetoC";
+import { ProjetoReact } from "./projetoReact";
 
 export function Projetos() {
   return (
@@ -26,77 +27,13 @@ export function Projetos() {
       </div>
 
       <div className="max-w-[1500px] mx-auto  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
-        
         <ProjetoJava />
-
-        {/* <Card className="cursor-pointer w-[300px] border-gray-500 hover:border-cyan-400 hover:shadow-[0_0_60px_rgba(34,211,238,0.8)] duration-200 hover:scale-90">
-          <CardHeader>
-            <CardTitle>
-              <div className="flex justify-center">
-                <Image src="/csharp.png" alt="c#" width={150} height={150} />
-              </div>
-            </CardTitle>
-            <CardDescription className="flex justify-center text-white text-2xl font-bold">
-              Projetos em C#
-            </CardDescription>
-          </CardHeader>
-        </Card> */}
 
         <ProjetoCsharp />
 
-        {/* <Card className="cursor-pointer w-full max-w-[300px] border-gray-500 hover:border-cyan-400 hover:shadow-[0_0_60px_rgba(34,211,238,0.8)] duration-200 hover:scale-90">
-          <CardHeader>
-            <CardTitle>
-              <div className="flex justify-center">
-                <Image src="/c.png" alt="c" width={150} height={150} />
-              </div>
-            </CardTitle>
-            <CardDescription className="flex justify-center text-white text-2xl font-bold">
-              Projetos em C
-            </CardDescription>
-          </CardHeader>
-        </Card> */}
         <ProjetoC />
 
-        <Card className="cursor-pointer w-[300px] border-gray-500 hover:border-cyan-400 hover:shadow-[0_0_60px_rgba(34,211,238,0.8)] duration-200 hover:scale-90">
-          <CardHeader>
-            <CardTitle>
-              <div className="flex justify-center">
-                <Image src="/react.png" alt="react" width={150} height={150} />
-              </div>
-            </CardTitle>
-            <CardDescription className="flex justify-center text-white text-2xl font-bold">
-              Projetos em React
-            </CardDescription>
-          </CardHeader>
-        </Card>
-      </div>
-
-      <div className="max-w-[1500px] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 place-items-center">
-        <div className="">
-          {/* <Image src="/java.png" alt="java" width={300} height={300} /> */}
-        </div>
-        <div className="">
-          {/* <Image src="/angular.png" alt="angular" width={150} height={150} /> */}
-        </div>
-        <div className="">
-          {/* <Image src="/css.png" alt="css" width={150} height={150} /> */}
-        </div>
-        <div className="">
-          {/* <Image src="/html.png" alt="html" width={150} height={150} /> */}
-        </div>
-        <div className="">
-          {/* <Image src="/react.png" alt="react" width={150} height={150} /> */}
-        </div>
-        <div className="">
-          {/* <Image src="/sqlserver.png" alt="sql" width={150} height={150} /> */}
-        </div>
-        <div className="">
-          {/* <Image src="/git.png" alt="git" width={150} height={150} /> */}
-        </div>
-        <div className="">
-          {/* <Image src="/github.jpg" alt="github" width={150} height={150} /> */}
-        </div>
+        <ProjetoReact />
       </div>
     </section>
   );
